019e83f6f7f829e4fb6b15a042a9902c3896459e
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2021-04-07 20:30:44 -0300

    Create DELETE request

--- backend/controller/todo-controller.js
+++ backend/controller/todo-controller.js
@@ -22,9 +22,24 @@ export const create = async (req, res) => {
   }
 };
 
-export const remove = async (req, res) => {};
+export const remove = async (req, res) => {
+  const { id } = req.params;
+  await TodoDB.deleteOne({ _id: id }, (error, { deletedCount }) => {
+    if (error) return res.status(500).send(error);
+    if (deletedCount === 0) return res.status(404).send(`${id} not found`);
+
+    const response = {
+      message: "Task removed successfully",
+      id,
+    };
+    return res.status(200).send(response);
+  });
+};
 
 export const get = async (req, res) => {
-  const allTasks = await TodoDB.find({});
-  res.send(allTasks);
+  await TodoDB.find({}, (error, allTasks) => {
+    if (error) return res.status(500).res.send(error);
+
+    return res.status(200).send(allTasks);
+  });
 };
