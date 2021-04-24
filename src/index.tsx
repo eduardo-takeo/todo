9d3b146e1d7c7c0bdfef420e99b18d2250bce791
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2021-04-23 22:12:06 -0300

    Remove redux from project

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import "./index.css";
 import HomePage from "./pages/HomePage";
-import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <HomePage />
-    </Provider>
+    <HomePage />
   </React.StrictMode>,
   document.getElementById("root")
 );
